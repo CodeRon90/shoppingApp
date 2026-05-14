5e1f93b820548301f1ced30d8743ca4eb768fea0
CodeRon90 <duffy.kjc@gmail.com>
2026-05-14 19:15:58 +0100

    first commit

---
+++ src/test/java/com/example/demo/Customer.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+public class Customer {
+    String name;
+    String emailAddress;
+    String customerID;
+    String password;
+    Boolean isProMember;
+    int phoneNumber;
+
+
+}
